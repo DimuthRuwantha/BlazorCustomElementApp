8b1479060407caa424afef8af11635a9f04ae610
PORT-HPCQST2\ADMIN <dimuthruwantha@gmail.com>
2023-06-22 12:09:00 +0100

    initial commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      {/* <my-counter id="myCounter" value="7" increment="2"></my-counter> */}
+      <my-counter id="myCounter"></my-counter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
